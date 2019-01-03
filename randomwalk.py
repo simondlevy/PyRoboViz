2982e12d31d9accaa1f12ec602b8b9d5ba63b493
simondlevy <simon.d.levy@gmail.com>
2019-01-03 16:20:29 -0500

    Random walk demo working

--- randomwalk.py
+++ randomwalk.py
@@ -23,20 +23,30 @@ along with this code.  If not, see <http:#www.gnu.org/licenses/>.
 
 from roboviz import Visualizer
 from time import sleep
+import numpy as np
 
 MAP_SIZE_PIXELS = 800
 MAP_SIZE_METERS = 32
+SPEED_MPS       = 10
+DT_SEC          = .01
 
 viz = Visualizer(MAP_SIZE_PIXELS, MAP_SIZE_METERS*1000/MAP_SIZE_PIXELS, 'Random Walk')
 
-pose = [16000,16000,0]
+pose = np.array([16000,16000,0])
 
 while True:
 
+    # Set current pose in visualizer
     viz.setPose(*pose)
 
     # Refresh the display, exiting gracefully if user closes it
     if not viz.refresh():
         exit(0)
 
-    sleep(.01) 
+    # Rotate randomly and move forward
+    pose[0] += 1000 * SPEED_MPS * DT_SEC * np.cos(np.radians(pose[2]))
+    pose[1] += 1000 * SPEED_MPS * DT_SEC * np.sin(np.radians(pose[2]))
+    pose[2] += 10 * np.random.randn()
+
+    # Hang a bit before next frame
+    sleep(DT_SEC)
