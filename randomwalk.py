3a855c64c34390f6dde7038181c735758d1865cd
simondlevy <simon.d.levy@gmail.com>
2019-01-12 21:19:41 -0500

    Use actual dt instead of constant

--- randomwalk.py
+++ randomwalk.py
@@ -22,19 +22,22 @@ along with this code.  If not, see <http:#www.gnu.org/licenses/>.
 
 
 from roboviz import Visualizer
-from time import sleep
+from time import time
 import numpy as np
 
 MAP_SIZE_PIXELS = 800
 MAP_SIZE_METERS = 32
-SPEED_MPS       = 10
-DT_SEC          = .01
+SPEED_MPS       = 1
+ZERO_ANGLE_DEG  = 0
 
 # Create a Visualizer object with a trajectory, centered at 0,0
-viz = Visualizer(MAP_SIZE_PIXELS, MAP_SIZE_METERS, 'Random Walk', True)
+viz = Visualizer(MAP_SIZE_PIXELS, MAP_SIZE_METERS, 'Random Walk', True, ZERO_ANGLE_DEG)
 
 # Start in the center of the map with a random heading
 pose = np.array([0,0,360*np.random.random()])
+
+# Start timing
+prevtime = time()
 
 # Loop till user closes the display
 while True:
@@ -47,11 +50,10 @@ while True:
         exit(0)
 
     # Rotate randomly and move forward
-    s = SPEED_MPS * DT_SEC 
+    currtime = time()
+    s = SPEED_MPS * (currtime - prevtime)
+    prevtime = currtime
     theta = np.radians(pose[2])
     pose[0] += s * np.cos(theta)
     pose[1] += s * np.sin(theta)
     pose[2] += 10 * np.random.randn()
-
-    # Hang a bit before next frame
-    sleep(DT_SEC)
